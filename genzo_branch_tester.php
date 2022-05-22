8c8639fa73e4a675c849e002875a7cc39fe39f69
Emanuel Schiendorfer <mandi.schiendorfer@gmail.com>
2022-05-22 21:30:20 +0200

    Fix sorting required

--- genzo_branch_tester.php
+++ genzo_branch_tester.php
@@ -50,7 +50,107 @@ class Genzo_Branch_Tester extends Module
 
 	// Backoffice
     public function getContent() {
-        return 'Genzo Branch Tester installed';
+
+        if (Tools::isSubmit('updateBranch')) {
+            $branch = pSQL(Tools::getValue('branch'));
+
+            if (!$this->replaceFilesByBranch($branch)) {
+                $this->errors[] = $this->l('Error occurred during copying files!');
+                return '';
+            }
+
+            if (!$this->{'update_branch_'.$branch}()) {
+                $this->errors[] = $this->l('Error occurred during update function!');
+                return '';
+            }
+
+            return $branch.' was updated!';
+        }
+
+        $this->context->smarty->assign([
+            'branches' => $this->getAllBranches(),
+        ]);
+
+        return $this->context->smarty->fetch(__DIR__ . '/views/templates/admin/form.tpl');
+    }
+
+    private function getAllBranches() {
+
+        $branches = [];
+
+        if ($h = opendir(__DIR__.'/branches')) {
+            while (($entry = readdir($h)) !== false) {
+
+                if ($entry==='.' || $entry==='..') {
+                    continue;
+                }
+
+                $branches[] =  $entry;
+            }
+            closedir($h);
+        }
+
+        return $branches;
+    }
+
+    private function replaceFilesByBranch($branch) {
+
+        $path = __DIR__.'/branches/'.$branch;
+
+        // Construct the iterator
+        $it = new RecursiveDirectoryIterator($path);
+
+        // Loop through files
+        foreach(new RecursiveIteratorIterator($it) as $file) {
+           if ($file->getExtension()) {
+
+               if (strpos($file, 'admin-dev')) {
+                   $new_file_path = str_replace($path.'/admin-dev', _PS_ADMIN_DIR_, $file);
+               }
+               else {
+                   $new_file_path = str_replace($path, _PS_CORE_DIR_, $file);
+               }
+
+               if (!copy($file, $new_file_path)) {
+                   return false;
+               }
+           }
+        }
+
+
+        return true;
+    }
+
+    // Specific update functions
+    private function update_branch_multiple_features() {
+
+        // Add feature_product_lang table
+        $prefix = _DB_PREFIX_;
+
+        $sql = "
+            CREATE TABLE IF NOT EXISTS `{$prefix}feature_product_lang` (
+                id_product       int                      not null,
+                id_feature_value int                      not null,
+                id_lang          int                      not null,
+                prefix           varchar(100)  default '' not null,
+                suffix           varchar(100)  default '' not null,
+                displayable      varchar(1000) default '' not null,
+            PRIMARY KEY (`id_product`, `id_feature_value`, `id_lang`)
+            ) ENGINE=_MYSQL_ENGINE_ DEFAULT CHARSET='utf8';
+        ";
+
+        if (!Db::getInstance()->execute($sql)) {
+            return false;
+        }
+
+        // Clean positions for all features
+        $features = Feature::getFeatures($this->context->language->id);
+        $ids_feature = array_column($features, 'id_feature');
+        foreach ($ids_feature as $id_feature) {
+            FeatureValue::cleanPositions($id_feature);
+        }
+
+        return true;
     }
 
 }
